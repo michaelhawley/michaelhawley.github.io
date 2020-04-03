0d4c0acab1396c266b65479c9fe6c65506011d0c
michaelhawley <michaelduncanhawley@gmail.com>
2020-04-03 13:19:53 +1300

    pop out filters bar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).foundation();
 
-     
+var showFilters = true;
+
+function showDiv() {
+    if (showFilters){
+    document.getElementById('disapear').style.display = "block";
+    showFilters = false;
+    }
+    else{
+        document.getElementById('disapear').style.display = "none";
+        showFilters = true;
+    }
+}
+
+
+  var containers = document.querySelectorAll(".container")[0];
+  function openfilterpane(){
+      console.log('c');
+      containers.classList.toggle("alert-is-shown");
+  }
