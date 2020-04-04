ae41dec8f84cc6f4b9249535f4574c7b5450c8bc
michaelhawley <michaelduncanhawley@gmail.com>
2020-04-04 20:59:57 +1300

    icons

--- js/app.js
+++ js/app.js
@@ -31,11 +31,13 @@ function openPullout(pane){
         case 'remove':
             $("#pulloutPaneContainer").children().remove();
             break;
-        case 'projection':      
+        case 'projection':
+            $("#pulloutPaneContainer").children().remove();      
             $("#pulloutPaneContainer").append('<div class = \"pulloutPane\"><span class=\"icon icon-globe\" onclick=\"make_projection(\'geoMercator\')\"></span><p class = \"globetext\"> Mercator </p><span class=\"icon icon-globe\" onclick=\"make_projection(\'geoOrthographic\')\"></span><p class = \"globetext\"> Orthographic </p><span class=\"icon icon-globe\" onclick=\"make_projection(\'geoAzimuthalEquidistant\')\"></span><p class = \"globetext\"> Equidistant (fixme) </p><span class=\"icon icon-globe\" onclick=\"make_projection(\'geoEquirectangular\')\"></span><p class = \"globetext\"> Equirectangular </p></div>');
             break;
         case 'paint':  
             $("#pulloutPaneContainer").children().remove();
+            $("#pulloutPaneContainer").append('<div class=\"pulloutPane\"><div class = \"colorDiv\"><span class=\"cicon cicon-grass\"></span><div class = \"colorSubDiv\"><p class = \"colorFont\">Land</p><input class=\"jscolor jscolor-size\" onchange=\"redraw_colors(jscolor)\" value=\"cc66ff\"></div></div><div class = \"colorDiv\"><span class=\"cicon cicon-border\"></span><div class = \"colorSubDiv\"><p class = \"colorFont\">Border</p><input class=\"jscolor jscolor-size\" onchange=\"redraw_border(jscolor)\" value=\"cc66ff\"></div></div><div class = \"colorDiv\"><span class=\"cicon cicon-wave\"></span><div class = \"colorSubDiv\"><p class = \"colorFont\">Ocean</p><input class=\"jscolor jscolor-size\"  value=\"cc66ff\"></div></div><div class = \"colorDiv\"><span class=\"cicon cicon-moon\"></span><div class = \"colorSubDiv\"><p class = \"colorFont\">Space</p><input class=\"jscolor jscolor-size\" onchange=\"redraw_background(jscolor)\" value=\"cc66ff\"></div></div><div class = \"colorDiv\"><span class=\"cicon cicon-hand\"></span><div class = \"colorSubDiv\"><p class = \"colorFont\">Select</p><input class=\"jscolor jscolor-size\" onchange=\"redraw_colors(jscolor)\" value=\"cc66ff\"></div></div></div>')
             break;
 
     }
